e5caa2ae3659cb18aa79b9eb163e91596b5afbb6
karlojean <jeankarlodev@gmail.com>
2024-05-01 20:23:49 -0300

    Resolvendo desafios de listagem com comparable e comparator
    Signed-off-by: karlojean <jeankarlodev@gmail.com>

---
+++ src/list/Ordenacao/ListaPessoas/OrdenacaoPessoa.java
@@ -0,0 +1,44 @@
+package list.Ordenacao.ListaPessoas;
+
+import javax.swing.border.Border;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+    private List<Pessoa> listaPessoa;
+
+    public OrdenacaoPessoa() {
+        this.listaPessoa = new ArrayList<>();
+    }
+
+    public void addPessoa(String nome, int idade, double altura) {
+        listaPessoa.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenadoPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(listaPessoa);
+
+        Collections.sort(pessoasPorIdade);
+
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(listaPessoa);
+        Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+
+        return pessoasPorAltura;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+        ordenacaoPessoa.addPessoa("Pedro", 32, 1.70);
+        ordenacaoPessoa.addPessoa("Claudia", 19, 1.65);
+        ordenacaoPessoa.addPessoa("Lucia", 29, 1.72);
+        ordenacaoPessoa.addPessoa("Petter", 18, 1.64);
+
+        System.out.println(ordenacaoPessoa.ordenadoPorIdade());
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+    }
+}
---
+++ src/list/Ordenacao/Numeros/OrdenacaoNumeros.java
@@ -0,0 +1,54 @@
+package list.Ordenacao.Numeros;
+
+import list.Ordenacao.ListaPessoas.OrdenacaoPessoa;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoNumeros {
+    private List<Integer> listaNumeros;
+
+    public OrdenacaoNumeros() {
+        this.listaNumeros = new ArrayList<>();
+    }
+
+    public void addNumero(int numero) {
+        this.listaNumeros.add(numero);
+    }
+
+    public List<Integer> ordenarAscendente() {
+        List<Integer> listaOrdenada = new ArrayList<>(this.listaNumeros);
+
+        Collections.sort(listaOrdenada);
+        return listaOrdenada;
+    }
+
+    public List<Integer> ordenarDescendente() {
+        List<Integer> listaOrdenada = new ArrayList<>(this.listaNumeros);
+
+        listaOrdenada.sort(Collections.reverseOrder());
+        return listaOrdenada;
+    }
+
+    public void listarNumeros(){
+        System.out.println(this.listaNumeros);
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoNumeros ordenacaoNumeros = new OrdenacaoNumeros();
+
+        ordenacaoNumeros.addNumero(8);
+        ordenacaoNumeros.addNumero(12);
+        ordenacaoNumeros.addNumero(90);
+        ordenacaoNumeros.addNumero(73);
+        ordenacaoNumeros.addNumero(1);
+        ordenacaoNumeros.addNumero(97);
+        ordenacaoNumeros.addNumero(-89);
+        ordenacaoNumeros.addNumero(123);
+
+        System.out.println(ordenacaoNumeros.ordenarAscendente());
+        System.out.println(ordenacaoNumeros.ordenarDescendente());
+
+    }
+}
